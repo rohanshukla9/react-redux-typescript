84826ede28da7ce1a53e9f694bf43a0adb1ae872
Rohan D. Shukla <rohanataglowid@gmail.com>
2021-06-22 12:56:58 +0530

    tailwindcss commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -44,7 +44,7 @@ class _App extends React.Component<AppProps> {
   // }
   render(){
     return (
-      <div className="h-100 w-full flex items-center justify-center bg-teal-300 font-sans">
+      <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
         <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
           <div className="mb-4">
               <h1 className="text-grey-darkest">Todo List</h1>  <button onClick={this.onButtonClick}>Fetch</button>
